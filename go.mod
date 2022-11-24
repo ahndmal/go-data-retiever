c6dc97d88592afea62d5694be3e589d36139ff06
Andrii Maliuta <quadr988@gmail.com>
2022-11-24 20:46:54 +0200

    init

--- go.mod
+++ go.mod
@@ -6,7 +6,14 @@ require (
 	github.com/PuerkitoBio/goquery v1.8.0 // indirect
 	github.com/andybalholm/cascadia v1.3.1 // indirect
 	github.com/fsnotify/fsnotify v1.5.4 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/jjeffcaii/reactor-go v0.5.5 // indirect
+	github.com/panjf2000/ants/v2 v2.6.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/rsocket/rsocket-go v0.8.12 // indirect
 	github.com/unrolled/render v1.5.0 // indirect
+	go.uber.org/atomic v1.10.0 // indirect
 	golang.org/x/net v0.0.0-20210916014120-12bc252f5db8 // indirect
 	golang.org/x/sys v0.0.0-20220615213510-4f61da869c0c // indirect
 )
--- main/main.go
+++ main/main.go
@@ -2,11 +2,9 @@ package main
 
 import (
 	"fmt"
+	"github.com/PuerkitoBio/goquery"
 	"log"
 	"net/http"
-	"strconv"
-
-	"github.com/PuerkitoBio/goquery"
 )
 
 type MnfChannel struct {
@@ -20,37 +18,22 @@ type MnfChannel struct {
 func main() {
 	response, err := http.Get("https://manifest.in.ua/rt/it/")
 	if err != nil {
-		fmt.Println(err)
 		log.Println(err)
 	}
+
 	body := response.Body
 	doc, err := goquery.NewDocumentFromReader(body)
 	if err != nil {
 		log.Println("Error when creating document from reader", err)
 	}
 	defer body.Close()
+
 	chanels := make([]MnfChannel, 0)
-	doc.Find("table.rating-table").
-		Find("tr").Find("td").Each(func(i int, td *goquery.Selection) {
-		chn := MnfChannel{}
-		switch i {
-		case 1:
-			chn.Title = td.Text()
-		case 3:
-			atoi, err := strconv.Atoi(td.Text())
-			if err != nil {
-				log.Println(err)
-			}
-			chn.Subscribers = atoi
-		case 4:
-			chn.Views = i
-		case 5:
-			chn.Videos = i
-		case 6:
-			chn.VideosPlusViews = i
-		}
-		chanels = append(chanels, chn)
-	})
+	trs := doc.Find("table.rating-table").
+		Find("tr")
+	for _, tr := range trs.Nodes {
+		fmt.Println(tr.FirstChild.Data)
+	}
 	//doc.Find("a.rating-table__channel-name").Each(func(i int, linkA *goquery.Selection) {
 	//	println(linkA.Text())
 	//})
